ecaf29ded1b44eadf205315d63cbae8339c8745e
Castrense Savojardo <castrense.savojardo2@unibo.it>
2023-06-12 14:45:59 +0200

    f

--- coconat/coconatconfig.py
+++ coconat/coconatconfig.py
@@ -12,6 +12,6 @@ CRF_BIN = "%s/tools/biocrf-static" % COCONAT_ROOT
 
 PROT_T5_MODEL = "%s/prot_t5_xl_uniref50" % COCONAT_PLM_DIR
 
-ESM_MODEL = "%s/esm2/esm2_t33_650M_UR50D.pt" % COCONAT_PLM_DIR
+ESM_MODEL = "%s/esm1b/esm1b_t33_650M_UR50S.pt" % COCONAT_PLM_DIR
 
 DEVICE = "cpu"
